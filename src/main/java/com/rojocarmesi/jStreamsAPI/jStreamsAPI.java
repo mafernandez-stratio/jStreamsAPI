3644a2b46020cae4afc9759cb09018fbfd6bd31e
miguel0afd <mafernandez@stratio.com>
2014-12-07 11:56:13 +0100

    Merge with local branch

---
+++ src/main/java/com/rojocarmesi/jStreamsAPI/jStreamsAPI.java
@@ -0,0 +1,13 @@
+package com.rojocarmesi.jStreamsAPI;
+
+/**
+ * Hello world!
+ * http://support.wan.travel/hc/en-us/articles/200191669-Wego-Flights-API
+ */
+public class jStreamsAPI
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
