c14707af723294c155aecde8a378f5abed65efec
miguel0afd <mafernandez@stratio.com>
2014-12-07 13:56:53 +0100

    Minor changes

--- src/main/java/com/rojocarmesi/jStreamsAPI/jStreamsAPI.java
+++ src/main/java/com/rojocarmesi/jStreamsAPI/jStreamsAPI.java
@@ -3,6 +3,7 @@ package com.rojocarmesi.jStreamsAPI;
 /**
  * Hello world!
  * http://support.wan.travel/hc/en-us/articles/200191669-Wego-Flights-API
+ * https://nycopendata.socrata.com/
  */
 public class jStreamsAPI
 {
